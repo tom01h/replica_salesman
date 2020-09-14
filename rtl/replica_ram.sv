1fc5ff30ff71761dd7f1996ced79706355a4f1cc
tom01h <tom01h@gmail.com>
2020-09-14 22:27:32 +0900

    replica ram bank

--- rtl/replica_ram.sv
+++ rtl/replica_ram.sv
@@ -4,23 +4,29 @@ module replica_ram
     input  logic           clk,
     input  logic           reset,
     input  replica_command command,
+    input  logic           wbank,
     input  replica_data    prev_data,
     input  replica_data    folw_data,
+    output logic           out_valid,
     output replica_data    out_data
 );
 
-replica_data [city_num-1:0]  ram;
-logic        [city_num-1:0]  count;
-logic        [city_num-1:0]  count_d1, count_d2;
-logic                        write_d1, write_d2;
-logic                        sel_d1,   sel_d2;
-replica_data                 out_data_r;
+replica_data [1:0][city_num-1:0]  ram;
 
-replica_data                 write_data;
+logic             [city_num-1:0]  count;
+logic             [city_num-1:0]  count_d1, count_d2;
+logic                             write_d1, write_d2;
+logic                             sel_d1,   sel_d2;
+logic                             rbank_d1, rbank_d2;
+
+replica_data                      write_data;
 assign write_data = (sel_d2) ? prev_data : folw_data;
 
+logic                             out_valid_r;
+replica_data                      out_data_r;
 always_ff @(posedge clk) begin
-    out_data <= out_data_r;
+    out_valid <= out_valid_r;
+    out_data  <= out_data_r;
 end
 
 always_ff @(posedge clk) begin
@@ -28,29 +34,36 @@ always_ff @(posedge clk) begin
     write_d1 <= (command != NOP || count != 0);
     if (command != NOP)
         sel_d1   <= (command == PREV);
+    rbank_d1 <= ~wbank;
     count_d2 <= count_d1;
     write_d2 <= write_d1;
     sel_d2   <= sel_d1;
+    rbank_d2 <= rbank_d1;
 end
 
 always_ff @(posedge clk) begin
     if (write_d2) begin
-        ram[count_d2] <= write_data;
+        ram[rbank_d2][count_d2] <= write_data;
     end
-    out_data_r <= ram[count];
+    out_data_r <= ram[wbank][count];
 end
 
 always_ff @(posedge clk) begin
     if (reset) begin
-        count <= '0;
+        count       <= '0;
+        out_valid_r <= '0;
     end else if (command != NOP) begin
         count <= count + 1;
-    end else if ( count != '0) begin    
+        out_valid_r <= '1;
+    end else if ( count != '0) begin
+        out_valid_r <= '1;
         if (count + 1 != city_num) begin
             count <= count + 1;
         end else begin
             count <= '0;
-        end        
+        end
+    end else begin
+        out_valid_r <= '0;
     end
 end    
 
