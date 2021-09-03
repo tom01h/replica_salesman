5fe2988d9ae1290214db3ab6e49aae571eb2717c
tom01h <tom01h@gmail.com>
2021-09-03 22:49:18 +0900

    ModelSim スクリプト

--- sim/build.sh
+++ sim/build.sh
@@ -7,18 +7,25 @@ rm -r __pycache__/  work cexports.obj cimports.dll dpiheader.h tb.obj
 
 vlib.exe work
 
-vlog.exe -sv -dpiheader dpiheader.h ../rtl/replica_pkg.sv tb.sv \
-    ../rtl/top.sv ../bus_if/bus_if.sv ../rtl/exp.sv ../rtl/node.sv \
-    ../rtl/sub_node.sv ../rtl/node_control.sv ../rtl/node_reg.sv \
-    ../rtl/random.sv  ../rtl/or_rand.sv  ../rtl/tw_rand.sv \
-    ../rtl/distance.sv ../rtl/metropolis.sv ../rtl/replica.sv ../rtl/replica_d.sv \
-    ../rtl/exchange.sv ../rtl/opt_route_or.sv ../rtl/opt_route_two.sv
+export BASE=".." # "U:\home\tom01h\work\replica_salesman"
+
+# cp ~/work/replica_salesman/sim/replica_salesman_sim.py .
+
+vlog.exe -sv -dpiheader   dpiheader.h            $BASE/rtl/replica_pkg.sv $BASE/sim/tb.sv\
+    $BASE/rtl/top.sv      $BASE/bus_if/bus_if.sv    $BASE/rtl/exp.sv      $BASE/rtl/node.sv \
+    $BASE/rtl/sub_node.sv $BASE/rtl/node_control.sv $BASE/rtl/node_reg.sv \
+    $BASE/rtl/random.sv   $BASE/rtl/or_rand.sv      $BASE/rtl/tw_rand.sv \
+    $BASE/rtl/distance.sv $BASE/rtl/metropolis.sv   $BASE/rtl/replica.sv  $BASE/rtl/replica_d.sv \
+    $BASE/rtl/exchange.sv $BASE/rtl/opt_route_or.sv $BASE/rtl/opt_route_two.sv
 vsim.exe tb -dpiexportobj cexports -c
 
 #/mnt/c/intelFPGA_pro/20.3/modelsim_ase/gcc-4.2.1-mingw32vc12/bin/
-g++.exe -c -g -I'C:/intelFPGA_pro/20.3/modelsim_ase/include/' tb.cpp -o tb.obj -I"C:/Users/tom01/AppData/Local/Programs/Python/Python36-32/include/"
+g++.exe -c -g -I'C:/intelFPGA_pro/20.3/modelsim_ase/include/' $BASE/sim/tb.cpp -o tb.obj -I"C:/Users/tom01/AppData/Local/Programs/Python/Python36-32/include/"
 g++.exe -shared -o cimports.dll tb.obj cexports.obj -L'C:/intelFPGA_pro/20.3/modelsim_ase/win32aloem' -lmtipli -L"C:/Users/tom01/AppData/Local/Programs/Python/Python36-32/libs/" -lpython36
 
 vsim.exe -c -sv_lib cimports tb -do " \
 add wave -noupdate /tb/* -recursive; \
+add wave -noupdate /tb/top/genblk1[0]/node/or_node/exchange/ram; \
 run 10us;quit -f"
+
+# rm replica_salesman_sim.py
