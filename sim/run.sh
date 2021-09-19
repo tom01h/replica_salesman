b230265dd4f0acdb7eb8cafa1fa009c0c79957b6
tom01h <tom01h@gmail.com>
2021-09-19 16:07:54 +0900

    忘れ

--- sim/run.sh
+++ sim/run.sh
@@ -3,6 +3,9 @@
 export WSLENV=PYTHONPATH/l
 export PYTHONPATH=$PWD
 
+# cp ~/work/replica_salesman/sim/replica_salesman_sim.py .
+# cp ~/work/replica_salesman/sim/top.py .
+
 dd if=/dev/zero of=tb.txt bs=1K count=1
 
 vsim.exe -c -sv_lib cimports tb -do " \
@@ -12,3 +15,5 @@ run -all;quit -f" > /dev/null &
 python.exe replica_salesman_sim.py
 
 rm tb.txt
+
+# rm replica_salesman_sim.py top.py
