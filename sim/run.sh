d5ca3ddcdb6f58eb7bb468c75876f0f51a37a9cc
tom01h <tom01h@gmail.com>
2021-09-19 16:02:44 +0900

    忘れ

--- sim/run.sh
+++ sim/run.sh
@@ -3,6 +3,12 @@
 export WSLENV=PYTHONPATH/l
 export PYTHONPATH=$PWD
 
+dd if=/dev/zero of=tb.txt bs=1K count=1
+
 vsim.exe -c -sv_lib cimports tb -do " \
 add wave -noupdate /tb/* -recursive; \
-run -all;quit -f"
+run -all;quit -f" > /dev/null &
+
+python.exe replica_salesman_sim.py
+
+rm tb.txt
