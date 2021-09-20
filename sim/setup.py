88ed3250b1322cffbb2f08d3b5ee668c929b6b35
tom01h <tom01h@gmail.com>
2021-09-20 14:13:09 +0900

    verilator オプション修正

--- sim/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, Extension
-import glob
-
-filelist  = ['top.cpp', 'verilated.cpp']
-filelist += glob.glob("sim/*.cpp")
-setup(name='top',
-        version='1.0',
-        ext_modules=[Extension('top', filelist)]
-)
